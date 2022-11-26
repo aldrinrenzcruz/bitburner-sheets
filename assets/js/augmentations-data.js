35f1ac071211d8a627947f86e77d59b7f8b86112
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-11-26 20:29:23 +0800

    Update augmentations-data.js

--- assets/js/augmentations-data.js
+++ assets/js/augmentations-data.js
@@ -248,7 +248,7 @@ const augmentsListTemplate = ({
 }) => `
 <div class="card rounded-0 mt-3 shadow">
   <div class="card-body">
-
+  <p class="card-text float-end"><small class="text-muted">Augment</small></p>
     <h6 class="card-title fw-bold">${augment}</h6>
     <p class="card-text"><small class="text-secondary">${description}</small></p>
     <p class="card-text"><strong>Source: </strong>${source}</p>
