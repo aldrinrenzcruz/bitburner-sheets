85b3b60ebf613f7254e295b5d4e1d1a2d03f792d
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-11-29 23:07:57 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function highlightFactionChecklist() {
             let checkbox = 'faction-checkbox-' + String(i);
             let card = 'faction-card-' + String(i);
             let data = `${localStorage.getItem(checkbox)}`
-            let bg = (data == 'true') ? "#ffffa3" : "inherit";
+            let bg = (data == 'true') ? "#ffff92" : "inherit";
             document.querySelector('#' + card).style.backgroundColor = bg;
             // if (data == 'true') {
             //     document.querySelector('#' + card).style.backgroundColor = "yellow";
@@ -67,7 +67,7 @@ function highlightAugmentChecklist() {
             let checkbox = 'augment-checkbox-' + String(i);
             let card = 'augment-card-' + String(i);
             let data = `${localStorage.getItem(checkbox)}`
-            let bg = (data == 'true') ? "#ffffa3" : "inherit";
+            let bg = (data == 'true') ? "#ffff70" : "inherit";
             document.querySelector('#' + card).style.backgroundColor = bg;
             // if (data == 'true') {
             //     document.querySelector('#' + card).style.backgroundColor = "yellow";
