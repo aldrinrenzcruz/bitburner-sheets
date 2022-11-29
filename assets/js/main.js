8afe41ad0241a51700440abd678417e9e10d0457
ixwrites <74097341+ixwrites@users.noreply.github.com>
2022-11-30 01:28:06 +0800

    fx

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,11 @@ $("#augment-cards-filter").keyup(function () {
 $('a').mousedown(function (e) {
     e.preventDefault();
 });
+
+// Bootstrap Tooltips Initialize
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Bootstrap Popover Initialize
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
